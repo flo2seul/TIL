1861cd7dd59b02b0cc3cde773523c7c84d49a2f0
seula <seulaayoon@gmail.com>
2023-10-08 11:27:34 +0900

    React 기본 2 - 배열 마무리

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -3,38 +3,8 @@ import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "윤슬아",
-//     content: "하이 1",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "구구구",
-//     content: "하이 3",
-//     emotion: 3,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "이이이",
-//     content: "하이 4",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 4,
-//     author: "따따",
-//     content: "하이 5",
-//     emotion: 2,
-//     created_date: new Date().getTime(),
-//   },
-// ];
 const App = () => {
-  const [data, setData] = useState("");
+  const [data, setData] = useState([]); // 배열로 초기값
 
   const dataId = useRef(0);
   const onCreate = (author, content, emotion) => {
@@ -49,10 +19,25 @@ const App = () => {
     dataId.current += 1;
     setData([newItem, ...data]);
   };
+
+  const onRemove = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다.`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+    console.log(`${targetId}가 수정되었습니다.`);
+  };
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryList onDelete={onRemove} onEdit={onEdit} diaryList={data} />
     </div>
   );
 };
--- simplediary/src/DiaryList.js
+++ simplediary/src/DiaryList.js
@@ -1,13 +1,12 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ diaryList }) => {
-  console.log(diaryList);
+const DiaryList = ({ onEdit, onRemove, diaryList }) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} />
+          <DiaryItem key={it.id} {...it} onEdit={onEdit} onDelete={onRemove} />
         ))}
       </div>
     </div>
